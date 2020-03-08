8cd5e53cea37834462903b3514cfc4ef9aab4614
Travis A. Everett <travis.a.everett@gmail.com>
2020-03-08 15:58:11 -0500

    fix format

--- test.sh
+++ test.sh
@@ -58,7 +58,7 @@
 # test/spec.sh oil-interactive --format=diffable
 # test/spec.sh oil-json --format=diffable
 # test/spec.sh oil-keywords --format=diffable
-test/spec.sh oil-keywords --format=diffable
+test/spec.sh oil-options -v --format=ansi
 # test/spec.sh oil-regex --format=diffable
 # test/spec.sh oil-slice-range --format=diffable
 # test/spec.sh oil-tuple --format=diffable
