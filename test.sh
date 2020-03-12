9617b9672fc4e4104a9b2a67dc9778458fb20ba3
Travis A. Everett <travis.a.everett@gmail.com>
2020-03-12 00:23:36 -0500

    add CI runs for macos/nix and move tests into script
    Adds multiple new test runs, which will be filled out by the upcoming commits.
    This also refactors the runs into segments so that they can be composed to
    minimize repetition where feasible.
    
    The runs include:
    - in-place linux build (the current CI run)
    - an in-place macOS build (this build is broken--it's just a placeholder
      since you indicated interest in a non-nix macOS build)
    - two pairs of macos/linux Nix builds (one w/ nix-shell, one w/ nix-build)
    
    Only the in-place linux build is required; none of the others will currently
    block build.

---
+++ test.sh
@@ -0,0 +1,18 @@
+# canonical test script; should probably be renamed
+# along with references in .travis.yml
+set -o nounset
+set -o pipefail
+set -o errexit
+
+test/lint.sh travis
+# Type checking with MyPy.  Problem: mypy requires Python 3, but Oil
+# requires Python 2.  The Travis environment doesn't like that.
+types/run.sh travis
+types/oil-slice.sh travis
+# Unit tests
+test/unit.sh all-for-minimal
+# Spec tests
+test/spec.sh smoke
+# Make sure dev build of yajl works
+test/spec.sh oil-json
+test/spec.sh interactive
