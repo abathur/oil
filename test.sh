0fce7f1cb9553601aca303288ab6e1f8a7bd41d9
Travis A. Everett <travis.a.everett@gmail.com>
2020-03-12 12:29:14 -0500

    enable full serial spec runs

--- test.sh
+++ test.sh
@@ -16,3 +16,7 @@ test/spec.sh smoke
 # Make sure dev build of yajl works
 test/spec.sh oil-json
 test/spec.sh interactive
+
+# Running serially is slow, but easier to debug ...
+test/spec.sh oil-all-serial
+test/spec.sh osh-all-serial
