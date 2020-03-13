344792e8a3c88475108917a98af7a5d3f0786c9d
Travis A. Everett <travis.a.everett@gmail.com>
2020-03-12 23:09:36 -0500

    _OVM_RESOURCE_ROOT A

--- test.sh
+++ test.sh
@@ -4,15 +4,18 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
-test/lint.sh travis
-# Type checking with MyPy.  Problem: mypy requires Python 3, but Oil
-# requires Python 2.  The Travis environment doesn't like that.
-types/run.sh travis
-types/oil-slice.sh travis
-# Unit tests
-test/unit.sh all-for-minimal
-# Spec tests
-test/spec.sh smoke
-# Make sure dev build of yajl works
-test/spec.sh oil-json
-test/spec.sh interactive
+echo inside!
+bin/osh --version
+
+# test/lint.sh travis
+# # Type checking with MyPy.  Problem: mypy requires Python 3, but Oil
+# # requires Python 2.  The Travis environment doesn't like that.
+# types/run.sh travis
+# types/oil-slice.sh travis
+# # Unit tests
+# test/unit.sh all-for-minimal
+# # Spec tests
+# test/spec.sh smoke
+# # Make sure dev build of yajl works
+# test/spec.sh oil-json
+# test/spec.sh interactive
